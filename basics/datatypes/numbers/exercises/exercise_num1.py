a397ccd416c316b875ed9ea54113ad853778659d
Sunjit Tara <sunjit@gmail.com>
2026-08-23 19:33:42 -0500

    exercises for numbers

---
+++ basics/datatypes/numbers/exercises/exercise_num1.py
@@ -0,0 +1,9 @@
+# Ask the user to enter two numbers and assign integer values to two numbers
+
+# if one is 11 and other is 4, how do you get 3 by dividing them
+
+# How do you get the remainder of their division
+
+# How do you do exponential
+
+# Short form of addition and subtraction
---
+++ basics/datatypes/numbers/exercises/exercise_num2.py
@@ -0,0 +1,5 @@
+# Try various examples of using the abs and round built in functions
+
+
+# Create complex numbers: Ex: 2+3j
+
